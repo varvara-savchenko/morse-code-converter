a45529ebaece00dccc1ea56835aca4f24eb18644
vs_mac <ping.varvara@gmail.com>
2024-02-18 20:31:32 +0100

    add more buttons

--- script.js
+++ script.js
@@ -51,9 +51,45 @@ function morseToText(morse) {
     }).join(" ")
 }
 
-document.getElementById("convertButton").addEventListener("click", function () {
-    const inputText = document.getElementById("textInput").value.trim();
-    const outputElement = document.getElementById("output");
+function copyText(outputElement) {
+    let copyButton = document.getElementById("copyButton");
+    if (copyButton === null) {
+        copyButton = document.createElement("button");
+        copyButton.textContent = "copy";
+        copyButton.id = "copyButton";
+
+        copyButton.addEventListener("click", function () {
+            navigator.clipboard.writeText(outputElement.textContent).then(function () {
+                alert("Text copied successfully!");
+            })
+        })
+        document.querySelector(".button-container").appendChild(copyButton);
+    }
+}
+
+function clearText() {
+    let clearButton = document.getElementById("clearButton");
+    if (clearButton === null) {
+        clearButton = document.createElement("button");
+        clearButton.textContent = "clear";
+        clearButton.id = "clearButton";
+        clearButton.addEventListener("click", function () {
+            document.getElementById("textInput").value = "";
+            document.getElementById("output").textContent = "";
+            elements = ["copyButton", "clearButton"];
+            elements.forEach((element) => {
+                document.getElementById(element).classList.add("hidden");
+            })
+        })
+        document.querySelector(".button-container").appendChild(clearButton);
+    } else {
+        document.querySelector(".button-container").appendChild(copyButton);
+        document.querySelector(".button-container").appendChild(clearButton);
+    }
+}
+
+function convertText(inputText) {
+    let outputElement = document.getElementById("output");
 
     if (inputText === "") {
         outputElement.textContent = "You must type something first.";
@@ -63,11 +99,20 @@ document.getElementById("convertButton").addEventListener("click", function () {
     } else if (/[a-z0-9]/i.test(inputText)) {
         const morseCode = textToMorse(inputText);
         outputElement.textContent = "Morse Code: " + morseCode;
-    }
-    else if (/^[.\- ]+$/.test(inputText)) {
+    } else if (/^[.\- ]+$/.test(inputText)) {
         const text = morseToText(inputText);
         outputElement.textContent = "Text: " + text;
     } else {
         outputElement.textContent = "Invalid characters.";
     }
+
+    if (outputElement.textContent !== "" && inputText !== "") {
+        copyText(outputElement);
+        clearText();
+    }
+}
+
+document.getElementById("convertButton").addEventListener("click", function () {
+    const inputText = document.getElementById("textInput").value.trim();
+    convertText(inputText);
 });
